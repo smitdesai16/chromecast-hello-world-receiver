fde6849525e11a23ac9ff1b7468fb9b8d186e47f
Smit Desai <smit.desai16@gmail.com>
2024-01-01 23:46:38 -0600

    Fixing base

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    base: "https://smitdesai16.github.io/chromecast-hello-world-receiver/"
 })
